aa00a8e07e7a4c10e0fc9b63a15086e400223001
OlesProkopiv <143633603+OlesProkopiv@users.noreply.github.com>
2025-11-17 16:13:55 +0200

    fix

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text(encoding="utf-8")
 
 setup(
-    name="my-sort",
+    name="my-sort-prokopiv",
     version="0.1.0",
     description="A small implementation of Unix sort (supports -r and -n) implemented with Click",
     long_description=README,
